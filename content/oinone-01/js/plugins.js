bb43a1b2985fa1828b78565a30155b0630864291
Fisnik Ajredini <fisnik.ajredini@kolabor.net>
2018-10-03 13:15:29 +0200

    beta 1

--- content/oinone-01/js/plugins.js
+++ content/oinone-01/js/plugins.js
@@ -53,32 +53,32 @@ $(window).on('load', function() {
     //*********************************************
 
         // Map Coordination - Map looks
-        var position = new google.maps.LatLng(47.195594, 8.814992);
+        //var position = new google.maps.LatLng(47.195594, 8.814992);
         // First Marker Coordination
-        var myposition = new google.maps.LatLng(47.195594, 8.814992);
+        //var myposition = new google.maps.LatLng(47.195594, 8.814992);
         // Map Options
-        var myOptions = {
-            zoom: 10,
-            center: position,
-            mapTypeId: google.maps.MapTypeId.ROADMAP,
-            disableDefaultUI: true,
-            scrollwheel: false,
+        //var myOptions = {
+        //    zoom: 10,
+        //   center: position,
+        //   mapTypeId: google.maps.MapTypeId.ROADMAP,
+        //   disableDefaultUI: true,
+        //    scrollwheel: false,
             // Google Map Color Styles
-            styles:
-            [{"featureType":"water","elementType":"geometry","stylers":[{"color":"#e9e9e9"},{"lightness":17}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"color":"#f5f5f5"},{"lightness":20}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#ffffff"},{"lightness":17}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#ffffff"},{"lightness":29},{"weight":0.2}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#ffffff"},{"lightness":18}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#ffffff"},{"lightness":16}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#f5f5f5"},{"lightness":21}]},{"featureType":"poi.park","elementType":"geometry","stylers":[{"color":"#dedede"},{"lightness":21}]},{"elementType":"labels.text.stroke","stylers":[{"visibility":"on"},{"color":"#ffffff"},{"lightness":16}]},{"elementType":"labels.text.fill","stylers":[{"saturation":36},{"color":"#333333"},{"lightness":40}]},{"elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"geometry","stylers":[{"color":"#f2f2f2"},{"lightness":19}]},{"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#fefefe"},{"lightness":20}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#fefefe"},{"lightness":17},{"weight":1.2}]}]
-        };
+        //    styles:
+        //    [{"featureType":"water","elementType":"geometry","stylers":[{"color":"#e9e9e9"},{"lightness":17}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"color":"#f5f5f5"},{"lightness":20}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#ffffff"},{"lightness":17}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#ffffff"},{"lightness":29},{"weight":0.2}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#ffffff"},{"lightness":18}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#ffffff"},{"lightness":16}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#f5f5f5"},{"lightness":21}]},{"featureType":"poi.park","elementType":"geometry","stylers":[{"color":"#dedede"},{"lightness":21}]},{"elementType":"labels.text.stroke","stylers":[{"visibility":"on"},{"color":"#ffffff"},{"lightness":16}]},{"elementType":"labels.text.fill","stylers":[{"saturation":36},{"color":"#333333"},{"lightness":40}]},{"elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"geometry","stylers":[{"color":"#f2f2f2"},{"lightness":19}]},{"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#fefefe"},{"lightness":20}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#fefefe"},{"lightness":17},{"weight":1.2}]}]
+        //};
         //Map ID
-        var map = new google.maps.Map(document.getElementById('google_map'), myOptions),
-            // Marker Images for first and second
-            image = 'images/marker2.png',
+        //var map = new google.maps.Map(document.getElementById('google_map'), myOptions),
+        //    // Marker Images for first and second
+        //    image = 'images/marker2.png',
             // Your Title and description
-            contentString =  '<h4>' + 'Headquarters' + '</h4><p>' + 'Of jobdesk®.' +'</p>',
+        //    contentString =  '<h4>' + 'Headquarters' + '</h4><p>' + 'Of jobdesk®.' +'</p>',
             //Marker Position and image selector
-            marker = new google.maps.Marker({ position: myposition, map: map, icon: image }),
+        //    marker = new google.maps.Marker({ position: myposition, map: map, icon: image }),
             // Infowindow
-            infowindow = new google.maps.InfoWindow({ content: contentString });
+        //    infowindow = new google.maps.InfoWindow({ content: contentString });
         //Click function for marker
-        google.maps.event.addListener(marker, 'click', function() { infowindow.open(map,marker); });
+        //google.maps.event.addListener(marker, 'click', function() { infowindow.open(map,marker); });
 
 // End Function
 });
